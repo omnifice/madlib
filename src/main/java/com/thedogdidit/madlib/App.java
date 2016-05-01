76bfb9afa155d5480e2811de3b3734da85bd613e
omnifice <omnifice@gmail.com>
2016-05-01 14:29:14 -0600

    Initial commit

---
+++ src/main/java/com/thedogdidit/madlib/App.java
@@ -0,0 +1,17 @@
+package com.thedogdidit.madlib;
+
+/**
+ * MadLibApp
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+
+        if (args.length != 3) {
+            println("Usage: )
+        }
+    }
+}
