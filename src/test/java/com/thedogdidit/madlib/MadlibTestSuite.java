da8575dce4bf8f98ad930b8b6b64a957eed81002
omnifice <omnifice@gmail.com>
2016-05-09 14:24:00 -0600

    Updated pom.xml, misc

--- src/test/java/com/thedogdidit/madlib/MadlibTestSuite.java
+++ src/test/java/com/thedogdidit/madlib/MadlibTestSuite.java
@@ -3,8 +3,6 @@ package com.thedogdidit.madlib;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({MadlibTest.class,
         HelpExceptionTest.class,
